69664fe57a6c92e7d12f87570038f0965f2139f3
LumreLars <gustavfl13@gmail.com>
2025-09-16 20:54:11 +0200

    Initial commit

---
+++ src/main/java/net/blueslime/moremetals/MoreMetals.java
@@ -0,0 +1,16 @@
+package net.blueslime.moremetals;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreMetals implements ModInitializer {
+	public static final String MOD_ID = "moremetals";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
