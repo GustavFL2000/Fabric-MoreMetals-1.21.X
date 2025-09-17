d7ca7437013ccaba60615de10a69081dd7b9b86a
LumreLars <gustavfl13@gmail.com>
2025-09-17 12:34:38 +0200

    Fixet fejl, med lang nullpointer. Stadig ingen texture eller navn

--- src/main/java/net/blueslime/moremetals/item/ModItems.java
+++ src/main/java/net/blueslime/moremetals/item/ModItems.java
@@ -1,5 +1,4 @@
 package net.blueslime.moremetals.item;
-
 import net.blueslime.moremetals.MoreMetals;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
@@ -8,9 +7,11 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+import static net.minecraft.item.Items.register;
+
 public class ModItems {
 
-    public static final Item SILVER_INGOT = registerItem("silver_ingot", new Item(new Item.Settings()));
+    public static final Item SILVER_INGOT = register("silver_ingot", Item::new, new Item.Settings());
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(MoreMetals.MOD_ID, name), item);
@@ -18,9 +19,9 @@ public class ModItems {
 
     public static void registerModItems() {
         MoreMetals.LOGGER.info("Registering Mod Items for " + MoreMetals.MOD_ID);
-
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(SILVER_INGOT);
         });
+
     }
 }
